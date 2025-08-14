eeac3dda95163a3fa6afd66b879b1d3a364e0fc6
Milla789 <milllaemanuelli@gmail.com>
2025-08-14 10:02:00 -0300

    Update Questão 02.c

--- Questão 02.c
+++ Questão 02.c
@@ -2,14 +2,19 @@
 
 int main() {
     int N;
-    int numero = 1; 
-    
+    int numero = 1;
+
     printf("Digite um número inteiro positivo: ");
     scanf("%d", &N);
-    for (int i = 1; i <= N; i++) { 
-        for (int j = 1; j <= i; j++) { 
+
+    for (int i = 1; ; i++) {  
+        for (int j = 1; j <= i; j++) {
+            if (numero > N) { 
+                printf("\n");
+                return 0;
+            }
             printf("%d ", numero);
-            numero++; 
+            numero++;
         }
         printf("\n");
     }
